2ccfe12babe8741740cb71fc4a6c4745bede1890
mav-rick <61046162+mav-rick@users.noreply.github.com>
2020-03-06 19:13:01 +0800

    updated

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour { 
+
+    public AudioSource button;
+    public AudioClip clickbutton;
+
+    public void ClickSound()
+    {
+        button.PlayOneShot(clickbutton);
+    }
+}
---
+++ Assets/Scripts/ToMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMain : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
